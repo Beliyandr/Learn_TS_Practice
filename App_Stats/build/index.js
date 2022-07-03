5784ccb843d05f6e23cd45024d0aa5ddb408c3de
beliyandr <beliyandr@gmail.com>
2022-07-03 16:35:39 +0300

    hardCoded + enum

--- App_Stats/build/index.js
+++ App_Stats/build/index.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log("HELLO");
+//! Enum - enumeration - перечисление - тип данных похожий на object
+//* Основная цель - дать понять другим разработчикам , что все эти значения тесно связаны
+//* Используется всякий раз, когда у нас есть небольшой фиксированный набор значений, которые все тесно связаны и известны во время компиляции
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default
+    .readFileSync("football.csv", {
+    encoding: "utf-8",
+})
+    .split("\n")
+    .map((row) => {
+    return row.split(",");
+});
+// Enum - enumeration - перечисление
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} times`);
--- App_Stats/src/index.ts
+++ App_Stats/src/index.ts
@@ -1,1 +1,33 @@
-console.log("HELLO");
+//! Enum - enumeration - перечисление - тип данных похожий на object
+//* Основная цель - дать понять другим разработчикам , что все эти значения тесно связаны
+//* Используется всякий раз, когда у нас есть небольшой фиксированный набор значений, которые все тесно связаны и известны во время компиляции
+
+import fs from "fs";
+
+const matches = fs
+  .readFileSync("football.csv", {
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
+
+// Enum - enumeration - перечисление
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} times`);
