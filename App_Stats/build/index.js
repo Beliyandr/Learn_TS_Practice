b0605dd2dd2897a8be1efe99eb0c52e3b4b294c2
beliyandr <beliyandr@gmail.com>
2022-07-10 21:59:29 +0300

    refactoring_2-CsvReader_composition

--- App_Stats/build/index.js
+++ App_Stats/build/index.js
@@ -1,8 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
 //! Enum - enumeration - перечисление - тип данных похожий на object
 //* Основная цель - дать понять другим разработчикам , что все эти значения тесно связаны
 //* Используется всякий раз, когда у нас есть небольшой фиксированный набор значений, которые все тесно связаны и известны во время компиляции
-Object.defineProperty(exports, "__esModule", { value: true });
 // import fs from "fs";
 // const matches = fs
 //   .readFileSync("football.csv", {
@@ -13,13 +14,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //     return row.split(",");
 //   });
 /****************************************************************************************************** */
-const MatchRaeder_1 = require("./MatchRaeder");
+const MatchReader_1 = require("./MatchReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new MatchRaeder_1.MatchRaeder("football.csv");
-reader.read();
+// const reader = new MatchRaeder("football.csv");
+// reader.read();
 // console.log(reader.data);
+//! 1. Create an object that implement the DataReader interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+//! 2. Create an object of MatchReader ans pass DataReader implementation
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches - сдесь храниться вся необходимая информация
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
--- App_Stats/src/index.ts
+++ App_Stats/src/index.ts
@@ -1,3 +1,4 @@
+import { CsvFileReader } from "./CsvFileReader";
 //! Enum - enumeration - перечисление - тип данных похожий на object
 //* Основная цель - дать понять другим разработчикам , что все эти значения тесно связаны
 //* Используется всякий раз, когда у нас есть небольшой фиксированный набор значений, которые все тесно связаны и известны во время компиляции
@@ -14,18 +15,28 @@
 
 /****************************************************************************************************** */
 
-import { MatchRaeder } from "./MatchRaeder";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
 
-const reader = new MatchRaeder("football.csv");
-
-reader.read();
-
+// const reader = new MatchRaeder("football.csv");
+// reader.read();
 // console.log(reader.data);
+
+//! 1. Create an object that implement the DataReader interface
+
+const csvFileReader = new CsvFileReader("football.csv");
+
+//! 2. Create an object of MatchReader ans pass DataReader implementation
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+// matchReader.matches - сдесь храниться вся необходимая информация
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
